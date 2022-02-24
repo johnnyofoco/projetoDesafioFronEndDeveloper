d836bb41ecfa31e7c6fbed8ebd9dce0310483e71
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-02-23 22:42:19 -0300

    Finalização da primeira versão do projeto

--- js/index.js
+++ js/index.js
@@ -9,21 +9,30 @@
   PENDÊNCIAS:
   FEITO! A) Carregar os elementos HTML;
   FEITO! B) Criar o(s) evento(s) listner(s);
-  C) Trabalhar na validação dos forms;
-  D) Criar o metódo que realizará a conexão para consumir a API;
-  F) Criar o metódo/função que escreve dinamicamente os elementos do containerProduct (tantos os iniciais como os  demais);
+  FEITO! C) Criar o metódo que realizará a conexão para consumir a API;
+  FEITO! D) Criar o metódo/função que escreve dinamicamente os elementos do containerProduct (tantos os iniciais como os  demais);
+  E) Trabalhar na validação dos forms;
 */
 
 //Elementos HTML
 const containerProduct = document.querySelector('.containerProduct')
 const addMoreProducts = document.querySelector('.addMoreProducts')
+let numberPage = 1
+let url = 'https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=' + numberPage
+const configs = {
+  method: 'GET',
+  header: {}
+}
+
+let firstLoad = false
 
 //Funções:
-
 /*########################################################*/
 
 //Função para carregar + 8 produtos:
-function loadProducts() {
+function loadProducts () {
+  firstLoad = true
+
   //Funções para tratar o retorno da API
   function transformToJson (response) {
     return response.json()
@@ -31,45 +40,46 @@ function loadProducts() {
 
   function exibirNaTela(dados) {
     
-    console.log(dados)
+    let products = dados.products
+    let nextPage = dados.nextPage
+
+    url = 'https://' + nextPage
+
+    for (let product of products) {
+      
+      containerProduct
+        .innerHTML += `<div class="product" id="${product.id}">
+                          <img src="${product.image}" alt="product Image">
+                          <h4>${product.name}</h4>
+                          <p>
+                            ${product.description}
+                          </p>
+                          <span id="from">
+                            De: ${product.oldPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }
+                          </span>
+                          <span id="by">
+                            Por: ${product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                          </span>
+                          <span id="or">
+                            ou ${product.installments.count}x de ${product.installments.value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                            </span>
+                          <button class="btnBuy">Comprar</button>
+                        </div>
+                        `
+    }
   }
-  function exibirErro (dados) {
-    console.log('Ops, deu erro!')
+
+  function exibirErro (error) {
+    console.log('Ops, deu erro! ' + error)
     const mainTitle = document.querySelector('main .title')
-    mainTitle.innerHTML += `<h2>Erro ao carregar os produtos</h2>`
+    mainTitle.innerHTML = `<h2>Erro ao carregar os produtos</h2>`
   }
 
-  //Trecho referente a conexão da API 
-  const configs = {
-    method: 'GET',
-    header: {}
-  }
-
-  const url = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
-
-
-  fetch(url,configs)
+  //Trecho referente a conexão da API
+  fetch(url, configs)
     .then(transformToJson)
     .then(exibirNaTela)
     .catch(exibirErro)
-
-  /*
-  for (let index = 0; index < 8; index++) {
-    console.log('cheguei aqui...')
-    containerProduct.innerHTML += `<div class="product">
-                    <img src="src/images/imgProductExample.png" alt="product Image">
-                    <h4>Nome do produto</h4>
-                    <p>
-                      Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.
-                    </p>
-                    <span id="from">De: R$23,99</span>
-                    <span id="by">Por: R$19,99</span>
-                    <span id="or">ou 2x de R$9,99</span>
-                    <button class="btnBuy">Comprar</button>
-                </div>
-                `
-    console.log('passei por aqui. tbm..')
-  }*/
 }
 
 //Listner´s
